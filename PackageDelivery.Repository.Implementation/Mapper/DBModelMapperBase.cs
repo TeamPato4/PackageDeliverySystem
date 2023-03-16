4d4b1b6b45c744a1f536ff30bdef3d0bf06782a6
Julian F <julian.1701913963@ucaldas.edu.co>
2023-03-16 14:50:21 -0500

    Agregar archivos de proyecto.

---
+++ PackageDelivery.Repository.Implementation/Mapper/DBModelMapperBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PackageDelivery.Repository.Implementation.Mapper
+{
+    public abstract class DBModelMapperBase<DbModelType, DatabaseType>
+    {
+        public abstract DbModelType DatabaseToDBModelMapper(DatabaseType input);
+        public abstract DatabaseType DBModelToDatabaseMapper(DbModelType input);
+        public abstract IEnumerable<DbModelType> DatabaseToDBModelMapper(IEnumerable<DatabaseType> input);
+        public abstract IEnumerable<DatabaseType> DBModelToDatabaseMapper(IEnumerable<DbModelType> input);
+    }
+}
