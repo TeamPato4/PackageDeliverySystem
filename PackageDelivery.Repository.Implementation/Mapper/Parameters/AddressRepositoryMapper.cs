0aa5b23edb86e52f7ddedaa47f305bb0d518a344
LuisLondon0 <luizlondono@gmail.com>
2023-03-29 17:06:25 -0500

    Creación de los DBModel y de los RepoitoryMapper de las clases Direccion, Departamento y Bodega

---
+++ PackageDelivery.Repository.Implementation/Mapper/Parameters/AddressRepositoryMapper.cs
@@ -0,0 +1,61 @@
+﻿using PackageDelivery.Repository.Contracts.DBModels.Parameters;
+using PackageDelivery.Repository.Implementation.DataModel;
+using System.Collections.Generic;
+
+namespace PackageDelivery.Repository.Implementation.Mapper.Parameters
+{
+    public class AddressRepositoryMapper : DBModelMapperBase<AddressDBModel, direccion>
+    {
+        public override AddressDBModel DatabaseToDBModelMapper(direccion input)
+        {
+            return new AddressDBModel()
+            {
+                Id = input.id,
+                StreetType = input.tipoCalle,
+                Number = input.numero,
+                PropertyType = input.tipoInmueble,
+                Neighborhood = input.barrio,
+                Observations = input.observaciones,
+                Current = input.actual,
+                IdTown = input.idMunicipio,
+                IdPerson = input.idPersona
+            };
+        }
+
+        public override IEnumerable<AddressDBModel> DatabaseToDBModelMapper(IEnumerable<direccion> input)
+        {
+            IList<AddressDBModel> list = new List<AddressDBModel>();
+            foreach (var item in input)
+            {
+                list.Add(this.DatabaseToDBModelMapper(item));
+            }
+            return list;
+        }
+
+        public override direccion DBModelToDatabaseMapper(AddressDBModel input)
+        {
+            return new direccion
+            {
+                id = input.Id,
+                tipoCalle = input.StreetType,
+                numero = input.Number,
+                tipoInmueble = input.PropertyType,
+                barrio = input.Neighborhood,
+                observaciones = input.Observations,
+                actual = input.Current,
+                idMunicipio = input.IdTown,
+                idPersona = input.IdPerson
+            };
+        }
+
+        public override IEnumerable<direccion> DBModelToDatabaseMapper(IEnumerable<AddressDBModel> input)
+        {
+            IList<direccion> list = new List<direccion>();
+            foreach (var item in input)
+            {
+                list.Add(this.DBModelToDatabaseMapper(item));
+            }
+            return list;
+        }
+    }
+}
---
+++ PackageDelivery.Repository.Implementation/Mapper/Parameters/WarehouseRepositoryMapper.cs
@@ -0,0 +1,61 @@
+﻿using PackageDelivery.Repository.Contracts.DBModels.Parameters;
+using PackageDelivery.Repository.Implementation.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageDelivery.Repository.Implementation.Mapper.Parameters
+{
+    public class WarehouseRepositoryMapper : DBModelMapperBase<WarehouseDBModel, bodega>
+    {
+        public override WarehouseDBModel DatabaseToDBModelMapper(bodega input)
+        {
+            return new WarehouseDBModel()
+            {
+                Id = input.id,
+                Name = input.nombre,
+                Code = input.codigo,
+                Address = input.direccion,
+                Latitude = input.latitud,
+                Longitude = input.longitud,
+                IdTown = input.idMunicipio
+            };
+        }
+
+        public override IEnumerable<WarehouseDBModel> DatabaseToDBModelMapper(IEnumerable<bodega> input)
+        {
+            IList<WarehouseDBModel> list = new List<WarehouseDBModel>();
+            foreach (var item in input)
+            {
+                list.Add(this.DatabaseToDBModelMapper(item));
+            }
+            return list;
+        }
+
+        public override bodega DBModelToDatabaseMapper(WarehouseDBModel input)
+        {
+            return new bodega
+            {
+                id = input.Id,
+                nombre = input.Name,
+                codigo = input.Code,
+                direccion = input.Address,
+                latitud = input.Latitude,
+                longitud = input.Longitude,
+                idMunicipio = input.IdTown
+            };
+        }
+
+        public override IEnumerable<bodega> DBModelToDatabaseMapper(IEnumerable<WarehouseDBModel> input)
+        {
+            IList<bodega> list = new List<bodega>();
+            foreach (var item in input)
+            {
+                list.Add(this.DBModelToDatabaseMapper(item));
+            }
+            return list;
+        }
+    }
+}
